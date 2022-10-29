f94394e4a195f7e658e0514a1a5f269a4c36fb5f
Alex Cheung <alexkwokoncheung@AC.local>
2022-10-29 04:13:33 -0700

    added intersection observe code

---
+++ app.js
@@ -0,0 +1,19 @@
+// create intersection observer for elements
+const observer = new IntersectionObserver((entries) => {
+  // the IntersectionObserver class takes this callback function in its constructor. When we start observing, we do observer.observe(entries)
+
+  entries.forEach((entry) => {
+    // If we want to have the animation happens only once, then we do not need the else statement. However, if we want the animation to be recurring, we need the else statement.
+
+    if (entry.isIntersecting) {
+      entry.classList.add("show");
+    } else {
+      entry.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+// tell the observer what elements to observe
+hiddenElements.forEach((el) => observer.observe(el));
